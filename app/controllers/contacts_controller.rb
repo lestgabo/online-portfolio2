9f98add10f1d3b039aee1a832a835873716ab081
Lestley Gabo <lestgabo@gmail.com>
2017-02-17 02:16:06 +0000

    Added custom Contact Lestley path

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,10 +7,10 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       flash[:success] = "Message sent."
-      redirect_to new_contact_path
+      redirect_to '/contact-lestley' #new_contact_path
     else
       flash[:danger] = @contact.errors.full_messages.join(", ")
-      redirect_to new_contact_path
+      redirect_to '/contact-lestley' #new_contact_path
     end
   end
   
