f3ed7f9ce269d7f4dfee4faa7af43afd257026f7
Lestley Gabo <lestgabo@gmail.com>
2017-03-12 04:05:23 +0000

    changed domain in config

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'shielded-river-55784.herokuapp.com',
+  :domain         => 'lestleygabo.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
