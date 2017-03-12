344f77ff914105d783bc48edd1ec61ff0ea7e43c
Lestley Gabo <lestgabo@gmail.com>
2017-03-12 05:20:27 +0000

    trying Sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,30 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
+  
+  
+  # added from learn-rails
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 2525,
+    domain: Rails.application.secrets.domain_name,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
+  }
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  # Send email in development mode?
+  config.action_mailer.perform_deliveries = true
+  # Send email in development mode?
+  config.action_mailer.perform_deliveries = true
+
+
+
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
