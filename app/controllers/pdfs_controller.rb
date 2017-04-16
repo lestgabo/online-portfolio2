0acc940c54ebfbdea1aa5ad266aea8b5289b62f5
Lestley Gabo <lestgabo@gmail.com>
2017-04-16 06:39:38 +0000

    adding more pdfs

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -2,7 +2,12 @@ class PdfsController < ApplicationController
   def resume
     pdf_filename = File.join(Rails.root, "public/2017 4 April 3.pdf")
     # send_file(pdf_filename, :filename => "your_document.pdf", :type => "application/pdf") # downloads pdf
-    send_file(pdf_filename, :filename => "public/2017 4 April 3.pdf", :disposition => 'inline', :type => "application/pdf") # show inline
+    @resume2 = send_file(pdf_filename, :filename => "public/2017 4 April 3.pdf", :disposition => 'inline', :type => "application/pdf") # show inline
+  end
+  
+  def ensc477lab4
+    pdf_filename = File.join(Rails.root, "public/ENSC477_Lab4_Rev1.1_finalcopy.pdf")
+    @resume2 = send_file(pdf_filename, :filename => "public/ENSC477_Lab4_Rev1.1_finalcopy.pdf", :disposition => 'inline', :type => "application/pdf") # show inline
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
   get 'contact-lestley', to: 'contacts#new', as: 'new_contact'
-  get '1', to: 'pdfs#resume'
+  get 'resume', to: 'pdfs#resume'
+  get 'ensc477lab4', to: 'pdfs#ensc477lab4'
+  get 'ensc477lab3', to: 'pdfs#ensc477lab3'
+  get 'ensc477lab2', to: 'pdfs#ensc477lab2'
+  get 'ensc477lab1', to: 'pdfs#ensc477lab1'
+  get 'ensc470lab3', to: 'pdfs#ensc470lab3'
 end
