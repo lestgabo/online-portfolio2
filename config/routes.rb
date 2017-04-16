de35f18409eb61d3f06736a0bc35cb4dd693a655
Lestley Gabo <lestgabo@gmail.com>
2017-04-16 08:19:04 +0000

    uploaded labs,projects

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,32 @@ Rails.application.routes.draw do
   resources :contacts, only: :create
   get 'contact-lestley', to: 'contacts#new', as: 'new_contact'
   get 'resume', to: 'pdfs#resume'
-  get 'ensc477lab4', to: 'pdfs#ensc477lab4'
-  get 'ensc477lab3', to: 'pdfs#ensc477lab3'
-  get 'ensc477lab2', to: 'pdfs#ensc477lab2'
-  get 'ensc477lab1', to: 'pdfs#ensc477lab1'
-  get 'ensc470lab3', to: 'pdfs#ensc470lab3'
+  
+  get 'ENSC483-project2', to: 'pdfs#ENSC483-project2'
+  get 'ENSC483-project1', to: 'pdfs#ENSC483-project1'
+  
+  get 'ENSC477-lab4', to: 'pdfs#ENSC477-lab4'
+  get 'ENSC477-lab3', to: 'pdfs#ENSC477-lab3'
+  get 'ENSC477-lab2', to: 'pdfs#ENSC477-lab2'
+  get 'ENSC477-lab1', to: 'pdfs#ENSC477-lab1'
+  
+  get 'ENSC476-lab3', to: 'pdfs#ENSC476-lab3'
+  get 'ENSC476-lab2', to: 'pdfs#ENSC476-lab2'
+  get 'ENSC476-lab1', to: 'pdfs#ENSC476-lab1'
+  
+  get 'ENSC470-lab3', to: 'pdfs#ENSC470-lab3'
+  get 'ENSC470-lab2', to: 'pdfs#ENSC470-lab2'
+  get 'ENSC470-lab1', to: 'pdfs#ENSC470-lab1'
+  
+  get 'ENSC424-project', to: 'pdfs#ENSC424-project'
+  
+  get 'ENSC406-project', to: 'pdfs#ENSC406-project'
+  get 'ENSC304-project', to: 'pdfs#ENSC304-project'
+  
+  get 'ENSC225-lab4', to: 'pdfs#ENSC225-lab4'
+  get 'ENSC225-lab3', to: 'pdfs#ENSC225-lab3'
+  get 'ENSC225-lab2', to: 'pdfs#ENSC225-lab2'
+  get 'ENSC225-lab1', to: 'pdfs#ENSC225-lab1'
+  
+  get 'PHYS131-lab1', to: 'pdfs#PHYS131-lab1'
 end
